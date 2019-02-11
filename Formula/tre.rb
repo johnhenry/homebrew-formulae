1959cfcfbcdf7e8ac62ad1881ba4be633467ccb5
Daniel Duan <daniel@duan.ca>
2019-02-10 16:16:22 -0800

    [tre] Bump to 0.0.2

--- Formula/tre.rb
+++ Formula/tre.rb
@@ -1,9 +1,9 @@
 class Tre < Formula
   desc "Git-friently replacement for the tree command."
   homepage "https://github.com/dduan/tre"
-  url "https://github.com/dduan/tre/archive/0.0.1.tar.gz"
-  sha256 "6415fcb3d40c1b18ee5fe70449fc7cfc457ee907a68bf1a80c3e3e331c78a51d"
-  version "0.0.1"
+  url "https://github.com/dduan/tre/archive/0.0.2.tar.gz"
+  sha256 "0093433db9101f5442c97b578ea8926a33639e82357024a9f89f5775ae104963"
+  version "0.0.2"
   depends_on :xcode => ["10.1", :build]
 
   def install
