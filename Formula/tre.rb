5366738fd6a786773ebc0f52f70d821229594696
Daniel Duan <daniel@duan.ca>
2019-04-13 20:26:29 -0700

    [tre] Release 0.1.3

--- Formula/tre.rb
+++ Formula/tre.rb
@@ -1,10 +1,11 @@
 class Tre < Formula
   desc "Git-friently replacement for the tree command."
   homepage "https://github.com/dduan/tre"
-  url "https://github.com/dduan/tre/archive/0.1.2.tar.gz"
-  sha256 "a4dbf623a7209a543abff6044d6c4f3738245679d8374a6144dbb494b8d3da4f"
-  version "0.1.2"
+  url "https://github.com/dduan/tre/archive/0.1.3.tar.gz"
+  sha256 "976051e994c79cc6178549b2a7c77170fba1713a0c339aa3c72c140c317045c0"
+  version "0.1.3"
   depends_on :xcode => ["10.2", :build]
+  head "https://github.com/dduan/tre.git", :shallow => false
 
   def install
     system "make", "build"
