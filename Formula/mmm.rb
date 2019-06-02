5f72d6788f0495abd9a1d29e3f1c6ffaea8104a2
Daniel Duan <daniel@duan.ca>
2019-06-02 00:31:25 -0700

    [mmm] Release 0.1.2

--- Formula/mmm.rb
+++ Formula/mmm.rb
@@ -1,14 +1,14 @@
 class Mmm < Formula
-  version '0.1.1'
+  version '0.1.2'
   desc "Your little interactive file manipulator"
   homepage "https://github.com/dduan/mmm"
 
   if OS.mac?
       url "https://github.com/dduan/mmm/releases/download/v#{version}/mmm-v#{version}-x86_64-apple-darwin.tar.gz"
-      sha256 "c229bc4a5b62e1060ba2baea1e1008b32e38ec6b1e84cb790cd7fcb3f6c911c2"
+      sha256 "3b29aa98635c813062b526308cc6ee02e7d090cc6b3458ab70b6d3c90956bc76"
   elsif OS.linux?
       url "https://github.com/dduan/mmm/releases/download/v#{version}/mmm-v#{version}-x86_64-unknown-linux-musl.tar.gz"
-      sha256 "3f9c2838d0bfa0364c40e00157a96ec7ac6c83233731657732a9795916cb26e3"
+      sha256 "b031ee757794f8dd69cb3d29ecde72e0570b1db330fd54bef001b0fcee56994c"
   end
 
   def install
