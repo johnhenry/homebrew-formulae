b4ffcd89fac16d16d462bc4f6ca8c3f5ee0e9c6a
Daniel Duan <daniel@duan.ca>
2019-06-09 20:59:28 -0700

    [tre] Release 0.2.0

--- Formula/tre.rb
+++ Formula/tre.rb
@@ -1,18 +1,17 @@
 class Tre < Formula
-  desc "Git-friently replacement for the tree command."
+  version '0.2.0'
+  desc "Tree command, improved."
   homepage "https://github.com/dduan/tre"
-  url "https://github.com/dduan/tre/archive/0.1.4.tar.gz"
-  sha256 "301396081d310334c76ae8795f43295e8b13fec298a06e29891a3feb2942e124"
-  version "0.1.4"
-  depends_on :xcode => ["10.2", :build]
-  head "https://github.com/dduan/tre.git", :shallow => false
 
-  def install
-    system "make", "build"
-    bin.install ".build/release/tre"
+  if OS.mac?
+      url "https://github.com/dduan/tre/releases/download/v#{version}/tre-v#{version}-x86_64-apple-darwin.tar.gz"
+      sha256 "4ae0e7aeedb4d9d2ed2a32de796869d18273446af4cb7623fceeff22955e6ef7"
+  elsif OS.linux?
+      url "https://github.com/dduan/tre/releases/download/v#{version}/tre-v#{version}-x86_64-unknown-linux-musl.tar.gz"
+      sha256 "f9da682ba5198f91ac174cff390afe3eda014486918610e88e13747f862c859e"
   end
 
-  test do
-    system "${bin}/tre"
+  def install
+    bin.install "tre"
   end
 end
