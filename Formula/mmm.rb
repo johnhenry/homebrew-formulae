7105670d0d813ea8d2cdf4ed175a37f488d1572e
Daniel Duan <daniel@duan.ca>
2019-08-06 13:53:40 -0700

    [mmm] Fix 0.1.4 shasum

--- Formula/mmm.rb
+++ Formula/mmm.rb
@@ -5,7 +5,7 @@ class Mmm < Formula
 
   if OS.mac?
       url "https://github.com/dduan/mmm/releases/download/v#{version}/mmm-v#{version}-x86_64-apple-darwin.tar.gz"
-      sha256 "37f5c8af7341c23703768aefd8d2ce40cc57cf517eb449c856f6d0feb7a72472"
+      sha256 "e3bbdd7dedc65ada8d297242e2c188c110b50917f42c9d77ec8ffc240dc7380f"
   elsif OS.linux?
       url "https://github.com/dduan/mmm/releases/download/v#{version}/mmm-v#{version}-x86_64-unknown-linux-musl.tar.gz"
       sha256 "05f613929edf1538049b8a1a305789982c9c9f503086c58070f8e1440e0c3465"
