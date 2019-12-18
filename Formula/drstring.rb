7cadbb18eceaa5d71275807c4f88dc3ccfebac8b
Daniel Duan <daniel@duan.ca>
2019-12-18 11:43:32 -0800

    [DrString] Release 0.3.2 (#8)

--- Formula/drstring.rb
+++ Formula/drstring.rb
@@ -1,14 +1,14 @@
 class Drstring < Formula
-   version '0.3.0'
+   version '0.3.2'
    desc "Linter, formatter for Swift docstrings."
    homepage "https://github.com/dduan/drstring"
 
    if OS.mac?
        url "https://github.com/dduan/DrString/releases/download/v#{version}/drstring-x86_64-apple-darwin.tar.gz"
-       sha256 "9c8a91bd7353736e5827e057f8b5583a3a056a2da3bb0b85f131a7ca0203ec1c"
+       sha256 "1cf945031f54a2467ac0d2631235faece45df23076a14d9cb5793c7af51a1a4f"
    elsif OS.linux?
        url "https://github.com/dduan/DrString/archive/v#{version}.tar.gz"
-       sha256 "448e1d2adc305585d165a7cb3cd20ad0dea1507f6c8e3c3cd222e8862b28e695"
+       sha256 "f89cfc00c3a892bee6e0c72228c7de160b996e63def131d534b109d3a307825f"
    end
 
    def install
